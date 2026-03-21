ffb0a636b5d02904ea0441aad8b970f6128511ba
Raphael Siqueira <106932282+raphaelsiqueiira@users.noreply.github.com>
2026-03-21 10:45:10 -0300

    Fix data.json path in AddResults initialization
    Updated the path to data.json in the AddResults initialization.

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ import AddResults from "./results.js";
 
 const summaryList = new AddResults(
   "#summary-item-container",
-  "../data.json",
+  "../../../data.json",
   "#results-score-achieved",
   '#results-rank',
   "#results-text"
